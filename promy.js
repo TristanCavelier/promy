c8b31a4d74cf5cccd6e4ee1417b048db0e94c766
Tristan Cavelier <t.cavelier@free.fr>
2014-04-11 23:10:40 +0200

    promy v1.0.1

--- promy.js
+++ promy.js
@@ -1,13 +1,17 @@
-// Copyright (c) 2014 Tristan Cavelier <t.cavelier@free.fr>
-// This program is free software. It comes without any warranty, to
-// the extent permitted by applicable law. You can redistribute it
-// and/or modify it under the terms of the Do What The Fuck You Want
-// To Public License, Version 2, as published by Sam Hocevar. See
-// the COPYING file for more details.
-
-// Promy
-// A Promise A+ compatible library with cancellation and notification
-// v1.0.0
+/*
+ * Promy
+ *
+ * A Promise A+ compatible library with cancellation and notification
+ *
+ * Version: v1.0.1
+ *
+ * Copyright (c) 2014 Tristan Cavelier <t.cavelier@free.fr>
+ * This program is free software. It comes without any warranty, to
+ * the extent permitted by applicable law. You can redistribute it
+ * and/or modify it under the terms of the Do What The Fuck You Want
+ * To Public License, Version 2, as published by Sam Hocevar. See
+ * the COPYING file for more details.
+ */
 
 /*jslint indent: 2, maxlen: 80, nomen: true */
 /*global setTimeout, setImmediate */
