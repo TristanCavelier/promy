1c173606e0e0905349152c14647ea8a4fb21f455
Tristan Cavelier <t.cavelier@free.fr>
2014-04-21 21:24:50 +0200

    promy v1.3.1

--- promy.js
+++ promy.js
@@ -9,7 +9,7 @@
    *
    * A Promise A+ compatible library with cancellation and notification
    *
-   * Version: v1.3.0
+   * Version: v1.3.1
    *
    * Copyright (c) 2014 Tristan Cavelier <t.cavelier@free.fr>
    * This program is free software. It comes without any warranty, to
